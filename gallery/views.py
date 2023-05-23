5db08a45b50b108aec3b2283e6ddb4418741822f
korsunmaksim <99172016+korsunmaksim@users.noreply.github.com>
2023-05-23 12:55:34 +0300

    add views

--- gallery/views.py
+++ gallery/views.py
@@ -1,1 +1,9 @@
 from django.shortcuts import render, get_object_or_404
+
+def gallery_view(request):   
+    return render(request, 'gallery.html')
+
+
+def image_detail(request,pk):
+    print(pk)
+    return render(request,"image_detail.html")

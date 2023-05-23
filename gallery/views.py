ec628661a514b4ee74eabf0d3435deb7c000fe5e
korsunmaksim <99172016+korsunmaksim@users.noreply.github.com>
2023-05-23 13:16:40 +0300

    added images

--- gallery/views.py
+++ gallery/views.py
@@ -1,9 +1,14 @@
+from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
+from .models import Category,Image
 
-def gallery_view(request):   
-    return render(request, 'gallery.html')
+def gallery_view(request):  
+    categories= Category.objects.all()
+    return render(request, 'gallery.html',{"categories":categories})
 
 
 def image_detail(request,pk):
-    print(pk)
-    return render(request,"image_detail.html")
+    image=Image.objects.filter(id=pk).first()
+    if not image:
+        return HttpResponse( "<h1>No image!</h1>")
+    return render(request,"image_detail.html",{"image":image})
